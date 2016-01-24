24621e22ca864b778af0ee0557ace701de385275
calebgregory <calebgregory@gmail.com>
2016-01-23 23:11:46 -0600

    Adding core logic - add, remove, toggle-complete, clear

--- src/clj_todo_cli/core.clj
+++ src/clj_todo_cli/core.clj
@@ -1,6 +1,35 @@
 (ns clj-todo-cli.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -main
+  [& args]
+  (println "received args: " args))
+
+(def counter (atom -1))
+(def todos (atom {}))
+
+(defrecord Todo [title completed])
+
+(defn make-todo
+  [title]
+  (->Todo title false))
+
+(defn add-todo
+  [title]
+  (let [todo (make-todo title)]
+    (dosync
+      (swap! counter inc)
+      (swap! todos assoc @counter todo))))
+
+(defn remove-todo
+  [id]
+  (swap! todos dissoc id))
+
+(defn toggle-todo
+  [id]
+  (swap! todos update-in [id :completed] not))
+
+(defn clear-all-todos
+  []
+  (dosync
+   (reset! counter -1)
+   (reset! todos {})))
--- test/clj_todo_cli/core_test.clj
+++ test/clj_todo_cli/core_test.clj
@@ -2,6 +2,32 @@
   (:require [clojure.test :refer :all]
             [clj-todo-cli.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn clean-todos
+  [f]
+  (swap! todos {})
+  (f))
+
+(use-fixtures :each clean-todos)
+
+(deftest adds-a-todo
+  (testing "Adding a todo to the list"
+    (is (= 0 (count @todos)))
+    (add-todo "Make todo")
+    (is (= 1 (count @todos)))))
+
+(deftest removes-a-todo
+  (testing "Removing a todo"
+    (add-todo "Make todo")
+    (is (= 1 (count @todos)))
+    (let [k (first (keys @todos))]
+      (remove-todo k))
+    (is (= 0 (count @todos)))))
+
+(deftest clears-all-todos
+  (testing "Clearing all the todos"
+    (add-todo "Make a todo")
+    (add-todo "Make another")
+    (is (= 2 (count @todos)))
+    (clear-all-todos)
+    (is (= 0 (count @todos)))
+    (is (= -1 @counter))))
